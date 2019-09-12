6627fbe37c14da42a122d61ec8a4cba122dabbb7
Oliver Crowe <olivercampbellcrowe@outlook.com>
2019-09-12 10:28:49 +0100

    Initital files

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Base meta tags, dont touch. -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Custom meta tags, change at will. -->
+        <title>Digital Leader Blog & News | South Bromsgrove High School</title>
+        <meta name="description" content="ENTER DESCRIPTION HERE">
+        <meta name="keywords" content="ENTER KEYWORDS HERE">
+        <meta name="author" content="Oliver Crowe">
+        <!-- Links, CSS, Fonts, Icons and frameworks -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet"> 
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+    
+    <!-- Javascript + JQuery -->
+    <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+	integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+	crossorigin="anonymous"></script>
+    <script src="js/index.js"></script>
+    </body>
+</html>
